3c0f7bb194dd83327eef8d22fb9fe90b370e0565
AsadbekSWE <asadbekgulomov2010@gmail.com>
2025-06-24 15:59:23 +0500

    chechkouting

---
+++ src/pages/about/AboutMe.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+
+const AboutCtn = styled.div`
+width: 100%;
+height: 1419px;
+background-color: #0f0f0f;
+
+`;
+
+
+export default function AboutMe() {
+    return (
+        <AboutCtn>
+            
+        </AboutCtn>
+    )
+}
